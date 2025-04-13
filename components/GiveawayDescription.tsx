3716008b2b3f7c11e118ac035951428d83fa6031
Isabella Teixeira <imt@bu.edu>
2025-04-12 20:11:57 -0400

    centered image thumbnails and styling descriptions

--- components/GiveawayDescription.tsx
+++ components/GiveawayDescription.tsx
@@ -1,9 +1,22 @@
+/*
+ * GiveawayDescription.tsx
+ * Component: GiveawayDescription
+ * Description:
+ *   - Displays a brief description of the giveaway in a styled paragraph
+ *
+ * Author: Isabella Teixeira
+ * Reasoning:
+ *   - Provides descriptions for each Giveaway
+ */
+
 import styled from "styled-components";
 
 const Desc = styled.p`
     font-size: 1rem;
     color: #333;
     margin: 0.75rem 0;
+    line-height: 1.5;
+    text-align: center;
 `;
 
 export default function GiveawayDescription({ description }: { description: string }) {
--- components/GiveawayThumbnail.tsx
+++ components/GiveawayThumbnail.tsx
@@ -5,6 +5,7 @@ const Image = styled.img`
     max-width: 300px;
     border-radius: 12px;
     margin-bottom: 1rem;
+    align-self: center;
 `;
 
 export default function GiveawayThumbnail({ thumbnail }: { thumbnail: string }) {
