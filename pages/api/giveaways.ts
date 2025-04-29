f5d359c1a2eefba6b7ea49ab4d505110bfb990bc
David Kaplansky <dkaplanskybrimmer@gmail.com>
2025-04-29 02:20:15 -0400

    minor update

--- pages/api/giveaways.ts
+++ pages/api/giveaways.ts
@@ -1,19 +1,22 @@
-export default async function handler(req: any, res: any) {
-    try {
-      const response = await fetch(
-        "https://www.gamerpower.com/api/giveaways?platform=steam&type=loot&sort-by=popularity"
-      );
-      
-      if (!response.ok) {
-        throw new Error("Failed to fetch giveaways");
-      }
-  
-      const data = await response.json();
-      res.status(200).json(data);
-      
-    } catch (error) {
-      console.error("API error:", error);
-      res.status(500).json({ error: "Upstream API error" });
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const response = await fetch(
+      "https://www.gamerpower.com/api/giveaways?platform=steam&type=loot&sort-by=popularity"
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch giveaways");
     }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("API error:", error);
+    res.status(500).json({ error: "Upstream API error" });
   }
-  
+}
